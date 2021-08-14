c254092295b57b5fbd8e01335df9bb49536e89db
Alexandr Shander <alexmasc05@gmail.com>
2021-08-14 06:45:33 +0000

    sed behavior in busybox different. I need to change index of first line from 0 to 1.

--- asterisk/entrypoint.sh
+++ asterisk/entrypoint.sh
@@ -5,7 +5,7 @@ counter=0
 while [ $counter -lt $default_password_count ]
 do
   newpassword=$(tr -dc '[:alnum:]' < /dev/urandom | fold -w 10| head -1)
-  sed -i "0,/password=password/s/password=password/password=$newpassword/" /etc/asterisk/pjsip_endpoints.conf
+  sed -i "1,/password=password/s/password=password/password=$newpassword/" /etc/asterisk/pjsip_endpoints.conf
   ((counter++))
 done
 
