e0876f5633d4270df4b52175a7aba6fb2ceb1853
Alexandr Shander <alexmasc05@gmail.com>
2021-08-14 06:38:06 +0000

    FIX a path of config files in the entrypoint.sh

--- asterisk/entrypoint.sh
+++ asterisk/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-default_password_count=$(grep -c 'password=password' configs/pjsip_endpoints.conf)
+default_password_count=$(grep -c 'password=password' /etc/asterisk/pjsip_endpoints.conf)
 counter=0
 while [ $counter -lt $default_password_count ]
 do
   newpassword=$(tr -dc '[:alnum:]' < /dev/urandom | fold -w 10| head -1)
-  sed -i "0,/password=password/s/password=password/password=$newpassword/" configs/pjsip_endpoints.conf
+  sed -i "0,/password=password/s/password=password/password=$newpassword/" /etc/asterisk/pjsip_endpoints.conf
   ((counter++))
 done
 
